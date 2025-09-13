120b1717fde4ae2136072afd6ebc6eb316632c73
hemalatha250 <Hselvam603@gmail.com>
2025-09-13 17:42:24 +0530

    Add files via upload
    DAy1

---
+++ week/Chrome.java
@@ -0,0 +1,17 @@
+package week;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float versionOfType = 91.0f;
+		String	developerOfType	=	"Google";
+		boolean	isBetaOfType	=	false;
+		int	releaseYearOfType	=	2008;
+		char	shortcutKeyOfType	='C';
+		System.out.println(versionOfType +"	"	+ developerOfType +" " + isBetaOfType	+ " "	+releaseYearOfType	+" " +shortcutKeyOfType);
+		
+		
+		
+	}
+
+}
---
+++ week/HelloTestLeaf.java
@@ -0,0 +1,11 @@
+package week;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to testleaf");
+
+	}
+
+}
